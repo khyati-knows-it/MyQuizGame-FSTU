51a31852e812cc93b5a0664a5400728d387d64f5
khyati-knows-it <73587478+khyati-knows-it@users.noreply.github.com>
2021-06-26 13:18:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+
+  //assign firebase database to database
+
+  // create an object of quiz class
+
+  // call getState() of quiz class
+
+  //call start() of quiz class
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
